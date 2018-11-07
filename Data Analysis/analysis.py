18189b45b17228228e192c270c12c3cc2cb7f8cf
ltcptgeneral <35508619+ltcptgeneral@users.noreply.github.com>
2018-11-06 21:42:49 -0600

    Add files via upload

--- Data Analysis/analysis.py
+++ Data Analysis/analysis.py
@@ -3,8 +3,15 @@
 import statistics
 import math
 import csv
-import random
 import functools
+
+class c_entities:
+
+    c_names = []
+    c_ids = []
+    c_pos = []
+    c_porperties = []
+    c_logic = []
 
 class nc_entities:
 
@@ -14,57 +21,107 @@ class nc_entities:
     c_properties = []
     c_effects = []
 
-class obstacles:
-
-    c_names = []
-    c_ids = []
-    c_pos = []
-    c_perim = []
-    c_effects = []
-
     def debug(self):
-        print("obstacles has atributes names, ids, positions, perimeters, and effects. __init__ takes self, 1d array of names, 1d array of ids, 2d array of position, 3d array of perimeters, 1d array of effects.")
-        return [c_names, c_ids, c_pos,c_perim, c_effects]
+        print ("nc_entities (non-controlable entities) has attributes names, ids, positions, properties, and effects. __init__ takes self, 1d array of names, 1d array of ids, 2d array of psoitions, 2d array of properties, and 2d array of effects.")
+        return[self.c_names, self.c_ids, self.c_pos, self.c_properties, self.c_effects]
 
-    def __init__ (self, names, ids, pos, perims, effects):
-        c_names = names
-        c_ids = ids
-        c_pos = pos
-        c_perims = perims
-        c_effects = effects
+    def __init__(self, names, ids, pos, properties, effects):
+        self.c_names = names
+        self.c_ids = ids
+        self.c_pos = pos
+        self.c_properties = properties
+        self.c_effects = effects
         return None
 
-    def append(self, n_name, n_id, n_pos, n_perim, n_effect):
-        c_names.append(n_name)
-        c_ids.append(n_id)
-        c_pos.append(n_pos)
-        c_perim.append(n_perim)
-        c_effects(n_effect)
-        return None
+    def append(self, n_name, n_id, n_pos, n_property, n_effect):
+        self.c_names.append(n_name)
+        self.c_ids.append(n_id)
+        self.c_pos.append(n_pos)
+        self.c_properties.append(n_property)
+        self.c_effects.append(n_effect)
 
-    def edit(self, search_id, n_name, n_id, n_pos, n_perim, n_effect):
+    def edit(self, search, n_name, n_id, n_pos, n_property, n_effect):
         position = 0
-        for i in c_ids:
-            if c_ids[i] == search:
+        for i in range(0, len(self.c_ids), 1):
+            if self.c_ids[i] == search:
                 position = i
-                
-        c_name[position] = n_name
+        if n_name != "null":
+            self.c_names[position] = n_name
 
         if n_id != "null":
-            c_id[position] = n_id
+            self.c_ids[position] = n_id
 
-        c_pos[position] = n_pos
-        c_perim[position] = n_perim
-        c_effect[position] = n_effect
+        if n_pos != "null":
+            self.c_pos[position] = n_pos
+
+        if n_property != "null":
+            self.c_properties[position] = n_property
+
+        if n_effect != "null":
+            self.c_effects[position] = n_effect
+
         return None
 
     def search(self, search):
         position = 0
-        for i in c_ids:
-            if c_ids[i] == search:
+        for i in range(0, len(self.c_ids), 1):
+            if self.c_ids[i] == search:
                 position = i
 
-        return [c_names[position], c_ids[position], c_pos[position], c_perim[position], c_effects[position]]
+        return [self.c_names[position], self.c_ids[position], self.c_pos[position], self.c_properties[position], self.c_effects[position]]        
+
+class obstacles:
+
+    c_names = []
+    c_ids = []
+    c_perim = []
+    c_effects = []
+
+    def debug(self):
+        print("obstacles has atributes names, ids, positions, perimeters, and effects. __init__ takes self, 1d array of names, 1d array of ids, 2d array of position, 3d array of perimeters, 2d array of effects.")
+        return [self.c_names, self.c_ids, self.c_perim, self.c_effects]
+
+    def __init__(self, names, ids, perims, effects):
+        self.c_names = names
+        self.c_ids = ids
+        self.c_perim = perims
+        self.c_effects = effects
+        return None
+
+    def append(self, n_name, n_id, n_perim, n_effect):
+        self.c_names.append(n_name)
+        self.c_ids.append(n_id)
+        self.c_perim.append(n_perim)
+        self.c_effects.append(n_effect)
+        return None
+
+    def edit(self, search, n_name, n_id, n_perim, n_effect):
+        position = 0
+        for i in range(0, len(self.c_ids), 1):
+            if self.c_ids[i] == search:
+                position = i
+
+        if n_name != "null":
+            self.c_names[position] = n_name
+
+        if n_id != "null":
+            self.c_ids[position] = n_id
+
+        if n_perim != "null":
+            self.c_perim[position] = n_perim
+
+        if n_effect != "null":
+            self.c_effects[position] = n_effect
+            
+        return None
+
+    def search(self, search):
+        position = 0
+        for i in range(0, len(self.c_ids), 1):
+            if self.c_ids[i] == search:
+                position = i
+
+        return [self.c_names[position], self.c_ids[position], self.c_perim[position], self.c_effects[position]]
 
 class objectives:
     
@@ -75,72 +132,84 @@ class objectives:
 
     def debug(self):
         print("objectives has atributes names, ids, positions, and effects. __init__ takes self, 1d array of names, 1d array of ids, 2d array of position, 1d array of effects.")
-        return [c_names, c_ids, c_pos, c_effects]
+        return [self.c_names, self.c_ids, self.c_pos, self.c_effects]
     
-    def __init__ (self, names, ids, pos, effects):
-        c_names = names
-        c_ids = ids
-        c_pos = pos
-        c_effects = effects
+    def __init__(self, names, ids, pos, effects):
+        self.c_names = names
+        self.c_ids = ids
+        self.c_pos = pos
+        self.c_effects = effects
         return None
     
     def append(self, n_name, n_id, n_pos, n_effect):
-        c_names.append(n_name)
-        c_ids.append(n_id)
-        c_pos.append(n_pos)
-        c_effects(n_effect)
+        self.c_names.append(n_name)
+        self.c_ids.append(n_id)
+        self.c_pos.append(n_pos)
+        self.c_effects.append(n_effect)
         return None
     
-    def edit(self, search_id, n_name, n_id, n_pos, n_effect):
+    def edit(self, search, n_name, n_id, n_pos, n_effect):
         position = 0
-        
-        for i in c_ids:
-            if c_ids[i] == search:
+        print(self.c_ids)
+        for i in range(0, len(self.c_ids), 1):
+            if self.c_ids[i] == search:
                 position = i
-        c_name[position] = n_name
+
+        if n_name != "null":
+            self.c_names[position] = n_name
 
         if n_id != "null":
-            c_id[position] = n_id
+            self.c_ids[position] = n_id
 
-        c_pos[position] = n_pos
-        c_effect[position] = n_effect
+        if n_pos != "null":
+            self.c_pos[position] = n_pos
+
+        if n_effect != "null":
+            self.c_effects[position] = n_effect
+            
         return None
     
     def search(self, search):
         position = 0
-        for i in c_ids:
-            if c_ids[i] == search:
+        for i in range(0, len(self.c_ids), 1):
+            if self.c_ids[i] == search:
                 position = i
 
-        return [c_names[position], c_ids[position], c_pos[position], c_effects[position]]
+        return [self.c_names[position], self.c_ids[position], self.c_pos[position], self.c_effects[position]]
 
 def load_csv(filepath):
     with open(filepath, newline = '') as csvfile:
         file_array = list(csv.reader(csvfile))
     return file_array
 
-def basic_stats(data, mode, arg): # data=array, mode = [0:1_basic_stats, 1:c_basic_stats, 2:r_basic_stats], arg for mode 1 or mode 2 for column or row
+def basic_stats(data, mode, arg): # data=array, mode = ['1d':1d_basic_stats, 'column':c_basic_stats, 'row':r_basic_stats], arg for mode 1 or mode 2 for column or row
 
     if mode == 'debug':
         out = "basic_stats requires 3 args: data, mode, arg; where data is data to be analyzed, mode is an int from 0 - 2 depending on type of analysis (by column or by row) and is only applicable to 2d arrays (for 1d arrays use mode 1), and arg is row/column number for mode 1 or mode 2; function returns: [mean, median, mode, stdev, variance]"
         return out
 
-    if mode == 0:
+    if mode == "1d" or mode == 0:
+
+        data_t = []
+
+        for i in range (0, len(data) - 1, 1):
+
+            data_t.append(float(data[i]))
     
-        mean = statistics.mean(data)
-        median = statistics.median(data)
+        mean = statistics.mean(data_t)
+        median = statistics.median(data_t)
         try:
-            mode = statistics.mode(data)
+            mode = statistics.mode(data_t)
         except:
             mode = None
-        stdev = statistics.stdev(data)
-        variance = statistics.variance(data)
+        stdev = statistics.stdev(data_t)
+        variance = statistics.variance(data_t)
         
         out = [mean, median, mode, stdev, variance]
 
         return out
     
-    elif mode == 1:
+    elif mode == "column" or mode == 1:
 
         c_data = []
         c_data_sorted = []
@@ -161,7 +230,7 @@ def basic_stats(data, mode, arg): # data=array, mode = [0:1_basic_stats, 1:c_bas
 
         return out
 
-    elif mode == 2:
+    elif mode == "row" or mode == 2:
 
         r_data = []
 
@@ -215,8 +284,8 @@ def histo_analysis(hist_data): #note: depreciated
         derivative.append(float(hist_data[i+1]) - float(hist_data[i]))
         
     derivative_sorted = sorted(derivative, key=int)
-    mean_derivative = basic_stats(derivative_sorted, 0, 0)[0]
-    stdev_derivative = basic_stats(derivative_sorted, 0, 0)[3]
+    mean_derivative = basic_stats(derivative_sorted, "1d", 0)[0]
+    stdev_derivative = basic_stats(derivative_sorted, "1d", 0)[3]
 
     low_bound = mean_derivative + -1.645 * stdev_derivative
     lm_bound = mean_derivative + -0.674 * stdev_derivative
@@ -243,8 +312,8 @@ def histo_analysis_2(hist_data, delta, low_bound, high_bound):
         derivative.append(float(hist_data[i + 1]) - float(hist_data [i]))
 
     derivative_sorted = sorted(derivative, key=int)
-    mean_derivative = basic_stats(derivative_sorted, 0, 0)[0]
-    stdev_derivative = basic_stats(derivative_sorted, 0, 0)[3]
+    mean_derivative = basic_stats(derivative_sorted,"1d", 0)[0]
+    stdev_derivative = basic_stats(derivative_sorted, "1d", 0)[3]
 
     predictions = []
     pred_change = 0
